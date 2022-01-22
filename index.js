f19c8304c522c721600a712fd9e6cc17891e0237
ritika600 <jawaritika24@gmail.com>
2022-01-22 22:17:23 +0530

    multiple pages

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const fs = require('fs');
 function requestHandler(req,res){
     console.log(req.url);
     res.writeHead(200,{'content-type':'text/html'})
-    fs.readFile('./index.html',function(err,data){
-        if(err){
-            console.log('error',err);
-            return res.end('<h1>Error!</h1>');
-        }
-        return res.end(data);
-    });
-    // res.end('<h1>Gotcha!</h1>');
+     let filePath;
+     switch(req.url){
+         case '/':
+             filePath = './index.html'
+             break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+        default:
+            filePath = './404.html'
+     }
+     fs.readFile(filePath,function(err,data){
+         if(err){
+             console.log('error',err);
+             return res.end('<h1>Error</h1>');
+         }
+         return res.end(data);
+     })
 }
 
 const server= http.createServer(requestHandler);
